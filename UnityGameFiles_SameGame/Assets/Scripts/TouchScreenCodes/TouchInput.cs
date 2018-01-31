66c64b73c24c864d2cae4abb7a90d1dfda6175df
carl69 <carlmorgenstierne@yahoo.com>
2018-01-31 19:06:45 +0100

    touch inputs

--- UnityGameFiles_SameGame/Assets/Scripts/TouchScreenCodes/TouchInput.cs
+++ UnityGameFiles_SameGame/Assets/Scripts/TouchScreenCodes/TouchInput.cs
@@ -4,13 +4,109 @@ using UnityEngine;
 
 public class TouchInput : MonoBehaviour {
 
+    public LayerMask TouchInputMask;
+    private List<GameObject> touchList = new List<GameObject>();
+    private GameObject[] touchesOld;
+
+    RaycastHit hit;
+
     void Update () {
-        if (Input.touchCount > 1)
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
         {
-            Application.Quit();
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+
+                if (Physics.Raycast(ray, out hit, TouchInputMask))
+                {
+                    GameObject recipient = hit.transform.gameObject;
+                    touchList.Add(recipient);
+
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                    if (Input.GetMouseButton(0))
+                    {
+                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            foreach (GameObject g in touchesOld)
+            {
+                if (!touchList.Contains(g))
+                {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
         }
 
-        Debug.Log(Input.touchCount);
-        	
+
+#endif
+
+
+
+
+
+
+
+
+        if (Input.touchCount > 0)
+        {
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+
+            foreach (Touch T in Input.touches)
+            {
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(T.position);
+                
+
+                if (Physics.Raycast(ray, out hit, TouchInputMask))
+                {
+                    GameObject recipient = hit.transform.gameObject;
+                    touchList.Add(recipient);
+
+
+                    if (T.phase == TouchPhase.Began)
+                    {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (T.phase == TouchPhase.Ended)
+                    {
+                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (T.phase == TouchPhase.Stationary || T.phase == TouchPhase.Moved)
+                    {
+                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (T.phase == TouchPhase.Canceled)
+                    {
+                        recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+            foreach (GameObject g in touchesOld)
+            {
+                if (!touchList.Contains(g))
+                {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }  	
 	}
 }
---
+++ UnityGameFiles_SameGame/Assets/button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+    public Color defultColour;
+    public Color selectedColour;
+    private Material mat;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+
+    }
+
+
+
+    void OnTouchDown()
+    {
+        mat.color = selectedColour;
+        print("Down");
+    }
+
+    void OnTouchUp()
+    {
+        mat.color = defultColour;
+        print("Up");
+    }
+
+    void OnTouchStay()
+    {
+        mat.color = selectedColour;
+        print("Staying");
+    }
+
+    void OnTouchExit()
+    {
+        mat.color = defultColour;
+        print("Exit");
+    }
+}
