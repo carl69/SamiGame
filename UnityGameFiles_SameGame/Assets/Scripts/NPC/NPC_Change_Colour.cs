899830f8e56e91e35363aefeb97a4ca1e59d153f
carl69 <carlmorgenstierne@yahoo.com>
2018-01-31 16:42:39 +0100

    testing for live touch in unity

---
+++ UnityGameFiles_SameGame/Assets/Scripts/NPC/NPC_Change_Colour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NPC_Change_Colour : MonoBehaviour {
+
+    public void TouchOn()
+    {
+        print("Something");
+        GetComponentInChildren<SpriteRenderer>().color = Color.red;
+    }
+}
---
+++ UnityGameFiles_SameGame/Assets/Scripts/TouchScreenCodes/TouchInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+    void Update () {
+        if (Input.touchCount > 1)
+        {
+            Application.Quit();
+        }
+
+        Debug.Log(Input.touchCount);
+        	
+	}
+}
