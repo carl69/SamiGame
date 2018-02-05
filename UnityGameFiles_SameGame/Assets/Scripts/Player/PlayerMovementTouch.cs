1eda1cb32d407290f7001ea9d6cd5a45b8df83e1
carl69 <carlmorgenstierne@yahoo.com>
2018-02-05 16:20:10 +0100

    cleaning up the errors

--- UnityGameFiles_SameGame/Assets/Scripts/Player/PlayerMovementTouch.cs
+++ UnityGameFiles_SameGame/Assets/Scripts/Player/PlayerMovementTouch.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovementTouch : MonoBehaviour {
     public float PlayerSpeed;
 void OnMove(Vector3 WalkPoint)
     {
--- UnityGameFiles_SameGame/Assets/Scripts/button.cs
+++ UnityGameFiles_SameGame/Assets/Scripts/button.cs
@@ -4,52 +4,52 @@ using UnityEngine;
 
 public class button : MonoBehaviour
 {
-	public Color defultColour;
-	public Color selectedColour;
-	private Material mat;
-	GameObject box_manager;
-	combo Combo;
+    public Color defultColour;
+    public Color selectedColour;
+    private Material mat;
+    GameObject box_manager;
+    combo Combo;
 
 
-	public string sendToNaidiSpeak;
-	public GameObject drum;
+    public string sendToNaidiSpeak;
+    public GameObject drum;
 
-	private void Start()
-	{
-		mat = GetComponent<Renderer>().material;
-		box_manager = GameObject.Find("box_manager");
-		Combo = box_manager.GetComponent<combo>();
-	}
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        box_manager = GameObject.Find("box_manager");
+        Combo = box_manager.GetComponent<combo>();
+    }
 
-	public void Update()
-	{
+    public void Update()
+    {
 
-	}
+    }
 
 
-	void OnTouchDown()
-	{
-		drum.GetComponent<Drum>().noaidiSpeak += sendToNaidiSpeak;
-		mat.color = selectedColour;
-		print("Down");
-		Combo.SendMessage("boxHit", this.gameObject, SendMessageOptions.DontRequireReceiver);
-	}
+    void OnTouchDown()
+    {
+        drum.GetComponent<Drum>().noaidiSpeak += sendToNaidiSpeak;
+        mat.color = selectedColour;
+        print("Down");
+        Combo.SendMessage("boxHit", this.gameObject, SendMessageOptions.DontRequireReceiver);
+    }
 
-	//void OnTouchUp()
-	//{
-	//	mat.color = defultColour;
-	//	print("Up");
-	//}
+    //void OnTouchUp()
+    //{
+    //    mat.color = defultColour;
+    //    print("Up");
+    //}
 
-	void OnTouchStay()
-	{
-		mat.color = selectedColour;
-		print("Staying");
-	}
+    void OnTouchStay()
+    {
+        mat.color = selectedColour;
+        print("Staying");
+    }
 
-	void OnTouchExit()
-	{
-		mat.color = defultColour;
-		print("Exit");
-	}
+    void OnTouchExit()
+    {
+        mat.color = defultColour;
+        print("Exit");
+    }
 }
