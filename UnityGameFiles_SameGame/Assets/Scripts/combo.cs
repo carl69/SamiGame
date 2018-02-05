eef7b553ccfd455135c1d11040c9ad349ccb2120
Cookies <hans.martin.h@hotmail.com>
2018-02-05 16:35:02 +0100

    Combo works

--- UnityGameFiles_SameGame/Assets/Scripts/combo.cs
+++ UnityGameFiles_SameGame/Assets/Scripts/combo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class combo : MonoBehaviour {
-
+	public string Victory;
 	// Use this for initialization
 	void Start () {
 		
@@ -21,48 +21,82 @@ public class combo : MonoBehaviour {
 		Debug.Log("ass" + box);
 		if (box.name == "Button1") 
 		{
+			Victory += "1 ";
 			print ("1");
 		}
 		if (box.name == "Button2")
 		{
+			Victory += "2 ";
 			print ("2");
 		}
 		if (box.name == "Button3")
 		{
+			Victory += "3 ";
 			print ("3");
 		}
 		if (box.name == "Button4")
 		{
+			Victory += "4 ";
 			print ("4");
 		}
 		if (box.name == "Button5")
 		{
+			Victory += "5 ";
 			print ("5");
 		}
 		if (box.name == "Button6")
 		{
+			Victory += "6 ";
 			print ("6");
 		}
 		if (box.name == "Button7")
 		{
+			Victory += "7 ";
 			print ("7");
 		}
 		if (box.name == "Button8")
 		{
-			print ("81");
+			Victory += "8 ";
+			print ("8");
 		}
 		if (box.name == "Button9")
 		{
+			Victory += "9 ";
 			print ("9");
+		}
+		if (box.name == "Button10")
+		{
+			Victory += "10 ";
+			print("10");
+		}
+		if (box.name == "Button11")
+		{
+			Victory += "11 ";
+			print("11");
+		}
+		if (box.name == "Button12")
+		{
+			Victory += "12 ";
+			print("12");
+		}
+		if (box.name == "Button13")
+		{
+			Victory += "13 ";
+			print("13");
+		}
+		if (box.name == "Button14")
+		{
+			Victory += "14 ";
+			print("14");
 		}
 	}
 
-	//void Victory()
-	//{
-	//	if (string s == "357")
-	//	{
-	//		Debug.Log("Voctory!");
-	//	}
-	//}
+	void Seier()
+	{
+		if (Victory.EndsWith ("12 3 12"))
+		{
+			Debug.Log("Voctory!");
+		}
+	}
 
 }
