f9e4df2dce819bc30212a98e8897bbb5ea3fcb5d
carl69 <carlmorgenstierne@yahoo.com>
2018-02-05 14:23:53 +0100

    working on touch point

---
+++ UnityGameFiles_SameGame/Assets/Scripts/WorldCodes/Ground/WalkPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPoint : MonoBehaviour {
+    GameObject Player;
+    PlayerMovement PMovement;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PMovement = Player.GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnTouchStay(Vector3 point)
+    {
+        
+        Debug.Log(point);
+    }
+}
